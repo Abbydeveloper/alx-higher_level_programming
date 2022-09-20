315d096e7d6fff4260d9a68f1e61833f6f54bf50
Adeboga Abigail <abbyboga@gmail.com>
2022-09-20 16:58:13 -0700

    Fix code to return the right answer when number is negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,9 +4,11 @@ import random
 
 number = random.randint(-10000, 10000)
 result = abs(number) % 10
+if number < 0:
+    result = result * -1
 if result > 5:
     print(f'Last digit of {number:d} is {result:d} and is greater than 5')
 elif result == 0:
     print(f'Last digit of {number:d} is {result:d} and is 0')
-elif result < 6 and result > 0:
+elif result < 6 and result != 0:
     print(f'Last digit of {number:d} is {result:d} and is less than 6 and not 0')
