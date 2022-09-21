fc701102ddb7f7a855980b40e693af60bd01b3f6
Adeboga Abigail <abbyboga@gmail.com>
2022-09-21 03:47:15 -0700

    Smile in the mirror

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for c in range(ord('z'), ord('a')-1, -1):
+    if c % 2 == 0:
+        print('{:c}{:s}'.format(c, chr(c), end='')
+    else:
+        print('{:c}{:s}'.format(c, chr(c - 33), end='')
