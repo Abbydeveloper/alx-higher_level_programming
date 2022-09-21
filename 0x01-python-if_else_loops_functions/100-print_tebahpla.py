b87ae75f49e9a273111a5bfabed3a967d08a36ac
Adeboga Abigail <abbyboga@gmail.com>
2022-09-21 03:54:17 -0700

    Fix requirement error

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
-for c in range(ord('z'), ord('a')-1, -1):
-    if c % 2 == 0:
-        print('{:c}{:s}'.format(c, chr(c), end=''))
-    else:
-        print('{:c}{:s}'.format(c, chr(c - 33), end=''))
+for c in range(ord('z'), ord('a')-1, -2):
+    print('{:c}{:s}'.format(c, chr(c - 33)), end='')
