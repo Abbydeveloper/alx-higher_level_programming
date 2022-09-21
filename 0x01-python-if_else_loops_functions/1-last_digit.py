24df17319121486ae821c595d53a5451335625e5
Adeboga Abigail <abbyboga@gmail.com>
2022-09-20 17:10:49 -0700

    Fix pycodestyle validation error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,12 +4,12 @@ import random
 
 number = random.randint(-10000, 10000)
 result = abs(number) % 10
+str = 'Last digit of'
 if number < 0:
     result = result * -1
 if result > 5:
-    print(f'Last digit of {number:d} is {result:d} and is greater than 5')
+    print(f'{str:s} {number:d} is {result:d} and is greater than 5')
 elif result == 0:
-    print(f'Last digit of {number:d} is {result:d} and is 0')
+    print(f'{str:s} {number:d} is {result:d} and is 0')
 elif result < 6 and result != 0:
-    print(f'Last digit of {number:d} is '
-         + f'{result:d} and is less than 6 and not 0')
+    print(f'{str:s} {number:d} is {result:d} and is less than 6 and not 0')
