ac94a95fbd34577aff7b507ef5302b49d2d26d6a
Adeboga Abigail <abbyboga@gmail.com>
2022-09-22 13:40:03 -0700

    Fix requirement errors

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from calculator_1.py import add, sub, mul, div
+if __name__ == "__main__":
+    from calculator_1.py import add, sub, mul, div
 
-a = 10
-b = 5
-print(f'{a:d} + {b:d} = {add(a, b):d}')
-print(f'{a:d} - {b:d} = {sub(a, b):d}')
-print(f'{a:d} * {b:d} = {mul(a, b):d}')
-print(f'{a:d} / {b:d} = {div(a, b):d}')
+    a = 10
+    b = 5
+    print(f'{a:d} + {b:d} = {add(a, b):d}')
+    print(f'{a:d} - {b:d} = {sub(a, b):d}')
+    print(f'{a:d} * {b:d} = {mul(a, b):d}')
+    print(f'{a:d} / {b:d} = {div(a, b):d}')
