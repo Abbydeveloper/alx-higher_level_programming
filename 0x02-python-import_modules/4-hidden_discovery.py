320a6337b145f0604acc065c53ff7d922cfc32d1
Adeboga Abigail <abbyboga@gmail.com>
2022-09-26 13:38:21 -0700

    Fix requirement error

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,5 @@ import hidden_4
 if __name__ == "__main__":
     hidden_names = dir(hidden_4)
     for c in hidden_names:
-        if name[:2} != "__":
+        if name[:2] != "__":
             print(c)
