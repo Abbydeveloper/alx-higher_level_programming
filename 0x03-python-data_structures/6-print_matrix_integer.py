2717a96c57eb18301b9c6f6ef41598e11499baa2
Adeboga Abigail <abbyboga@gmail.com>
2022-09-27 21:54:24 -0700

    Fix requirement error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,6 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for column in matrix:
+        for column in row:
             print('{:d}'.format(column), end=" " if column != row[-1] else '')
         print()
