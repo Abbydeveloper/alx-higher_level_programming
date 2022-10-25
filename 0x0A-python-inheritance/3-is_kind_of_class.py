694069df7ef25246495a4d9c47122e92caa31d50
Adeboga Abigail <abbyboga@gmail.com>
2022-10-25 13:59:32 -0700

    Is kind of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Module 3 - Is kind of class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if object obj is an instance of the class a_class or any of its children"""
+
+    return isinstance(obj, a_class)
