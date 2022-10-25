6fb7903f0944e80e5ad4142b0e7d1cc1373d242f
Adeboga Abigail <abbyboga@gmail.com>
2022-10-25 14:37:15 -0700

    Base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Module 6 - More Base Geometry"""
+
+
+class BaseGeometry:
+    """Base Geometry class"""
+
+    def area(self):
+        """Raises exception"""
+
+        raise Exception('area() is not implemented')
