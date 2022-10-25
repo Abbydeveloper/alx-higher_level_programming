3eaa1940efd10954b327393bb391eddcdd330661
Adeboga Abigail <abbyboga@gmail.com>
2022-10-24 17:53:04 -0700

    Add doc

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,8 +6,11 @@ Create a class to inherit from the list class
 
 
 class MyList(list):
+    """Implement sorting for lists."""
+
 
     def print_sorted(self):
+        """Sort  a list in ascending order and print"""
         new_list = self[:]
         new_list.sort()
         print("{}".format(new_list))
