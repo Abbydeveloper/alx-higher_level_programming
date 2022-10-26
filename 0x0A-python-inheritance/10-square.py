bf4991569e01d3e6b327c189f6a249468fcb9c72
Adeboga Abigail <abbyboga@gmail.com>
2022-10-26 01:16:34 -0700

    Debug 10-square module

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,7 +6,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-clase Square(Rectangle):
+class Square(Rectangle):
     """Square class, inherits from Rectangle"""
 
     def __init__(self, size):
@@ -14,7 +14,7 @@ clase Square(Rectangle):
 
         self.integer_validator("size", size)
         super().__init__(size, size)
-        self._size = size
+        self.__size = size
 
     def __str__(self):
         return super().__str__()
