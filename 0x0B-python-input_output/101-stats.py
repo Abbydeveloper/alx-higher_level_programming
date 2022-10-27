19156d45371359c29c680f25bf88e258593bc46b
Adeboga Abigail <abbyboga@gmail.com>
2022-10-26 19:10:14 -0700

    Module 101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -8,6 +8,14 @@ import sys
 status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 stat = {}
 count = 0
+size = 0
+
+
+def print_stats(size, stat):
+    """Print statistics"""
+    print("File size: {}".format(size))                                        
+    for key in sorted(stat):
+        print("{}: {}".format(key, stat[key]))
 
 try:
     for line in sys.stdin:
@@ -37,16 +45,6 @@ try:
 
         print_stats(size, stat)
 
-    except KeyboardInterupt:
-        print_stats(size, stat)
-        raise
-
-def print_stats(size, stat):
-    """Print statistics"""
-    print("File size: {}".format(size))
-    for key in sorted(stat):
-        print("{}: {}".format(key, stat[key]))
-
-
-
-
+except KeyboardInterrupt:
+    print_stats(size, stat)
+    raise
