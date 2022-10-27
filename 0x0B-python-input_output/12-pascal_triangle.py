106d37d7344c1d19c82da75cb0aad132f8b94fba
Adeboga Abigail <abbyboga@gmail.com>
2022-10-26 18:07:56 -0700

    Fix syntax error

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -11,12 +11,12 @@ def pascal_triangle(n):
     pascal_list = [[0 for a in range(i + 1)] for i in range(n)]
     pascal_list[0] = [1]
 
-    for i in range(pascal_list, n):
+    for i in range(1, n):
         pascal_list[i][0] = 1
-        for j in range(pascal_list, i + 1):
+        for j in range(1, i + 1):
             if j < len(pascal_list[i - 1]):
-                pascal_list[i][j] = pascal_list[i - 1][j - 1]
-                + pascal_list[i - 1][j]
+                pascal_list[i][j] = pascal_list[i - 1][j - 1] +\
+                pascal_list[i - 1][j]
             else:
                 pascal_list[i][j] = pascal_list[i - 1][0]
     return pascal_list
