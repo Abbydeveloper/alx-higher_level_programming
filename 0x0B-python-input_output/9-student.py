73a5bcda26f8679c888f96fba4a9742f994a47f5
Adeboga Abigail <abbyboga@gmail.com>
2022-10-26 17:54:17 -0700

    Fix style error

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,14 +5,12 @@
 class Student:
     """Student class"""
 
-    
     def __init__(self, first_name, last_name, age):
         """Initialize instance of Student class"""
 
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
 
     def to_json(self):
         """Get dict representation of Student instance"""
