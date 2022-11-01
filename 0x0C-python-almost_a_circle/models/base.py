b37a109e27d4f4f7893f62de266b3f7c883ab3cb
Adeboga Abigail <abbyboga@gmail.com>
2022-11-01 15:35:54 -0700

    Add base file

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Base class for all other classes in this project.
+It manages ghe id atribute in all future classes and avoids duplication of the same code (by extension, same bugs)"""
+
+
+class Base:
+    """Base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """class constructor"""
+
+        if id is not None:
+           self.id = id
+        else:
+            __nb_objects += 1
+            self.id = Base.__nb_objects
