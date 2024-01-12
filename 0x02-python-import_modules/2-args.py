e45292986c364769f07e7b3382fecdd85ad706a2
Adeboga Abigail <abbyboga@gmail.com>
2024-01-12 11:41:52 -0800

    Command line arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import sys
+
+length = len(sys.argv) - 1
+str = 0
+if (length == 0):
+    str = "s."
+elif (length > 1):
+    str = "s:"
+else:
+    str = ":"
+
+print("{} argument{}".format(length, str))
+
+for n in range(1, length + 1):
+    if (length > 0):
+        print("{}: {}".format(n, sys.argv[n]))
+
