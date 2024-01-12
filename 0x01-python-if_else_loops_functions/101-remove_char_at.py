73da28063259397c76394a1c96c815622448ff1f
Adeboga Abigail <abbyboga@gmail.com>
2024-01-12 03:17:11 -0800

    Remove at position

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    new_str = ""
+    length = len(str)
+    for i in range(0, length):
+        if (n != i):
+            new_str += str[i]
+
+    print("{}".format(new_str))
