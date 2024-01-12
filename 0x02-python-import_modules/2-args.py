060d9f2f45a54ddb51df34adc9070b93d71ca1b8
Adeboga Abigail <abbyboga@gmail.com>
2024-01-12 11:43:32 -0800

    Fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-length = len(sys.argv) - 1
-str = 0
-if (length == 0):
-    str = "s."
-elif (length > 1):
-    str = "s:"
-else:
-    str = ":"
+    length = len(sys.argv) - 1
+    str = 0
+    if (length == 0):
+        str = "s."
+    elif (length > 1):
+        str = "s:"
+    else:
+        str = ":"
 
-print("{} argument{}".format(length, str))
+    print("{} argument{}".format(length, str))
 
-for n in range(1, length + 1):
-    if (length > 0):
-        print("{}: {}".format(n, sys.argv[n]))
+    for n in range(1, length + 1):
+        if (length > 0):
+            print("{}: {}".format(n, sys.argv[n]))
 
