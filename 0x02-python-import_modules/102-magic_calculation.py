8eacdac5277a0ac66458375632e69ec84962f1ac
Adeboga Abigail <abbyboga@gmail.com>
2024-01-14 12:36:22 -0800

    Magic calculation

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from magic_calculation_102 import add, sub
+def magic_calculation(a, b):
+    if (a < b):
+        c = add(a, b)
+
+        for a in range(0, 90):
+            c += i
+
+        return c
+    else:
+        return sub(a, b)
