3759cb55b3881ede38d709eea3cf3c8cd8fd4221
Adeboga Abigail <abbyboga@gmail.com>
2024-01-17 07:47:29 -0800

    Fixed

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,11 +4,10 @@ def new_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
     length = len(my_list)
-    if idx > length:
+    if idx >= length:
         return my_list
     new_list = my_list.copy()
     if new_list[idx]:
-            new_list[idx] = element
-
+        new_list[idx] = element
 
     return new_list
