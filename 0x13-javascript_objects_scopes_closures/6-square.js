e73b6b4a02eda0b8aabba45cff1a1b922c7a741a
Adeboga Abigail <abbyboga@gmail.com>
2024-04-09 16:44:53 -0700

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const ParentSquare = require('./5-square');
+
+class Square extends ParentSquare {
+  charPrint(c) {
+    let squareChar = c;
+
+    if (c === undefined) {
+      squareChar = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(squareChar.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
