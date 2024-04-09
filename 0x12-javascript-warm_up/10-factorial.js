937602f799b049a3bad4ff3869fa1fe2e4d5c278
Adeboga Abigail <abbyboga@gmail.com>
2024-04-08 17:28:00 -0700

    Fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -5,7 +5,7 @@ const a = process.argv[2];
 function factorial (x) {
   if (x < 0) {
     return (-1);
-  } else if (a == 0 || isNaN(a) || a === undefined) {
+  } else if (a === 0 || isNaN(a) || a === undefined) {
     return (1);
   }
   return (x * factorial(x - 1));
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   const a = process.argv.slice(2).map(Number);
-  const sortedArr = a.sort((x, y) => {return (y - x)});
-  console.log(sortedArr[1])
+  const sortedArr = a.sort((x, y) => { return (y - x); });
+  console.log(sortedArr[1]);
 }
