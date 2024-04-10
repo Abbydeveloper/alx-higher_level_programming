42c8e0c9f746f314e3949dfb465b9b082a083760
Adeboga Abigail <abbyboga@gmail.com>
2024-04-09 17:40:40 -0700

    Sorted occurences

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const data = require('./101-data').dict;
+
+const newDict = {};
+
+for (const [key, value] of Object.entries(data)) {
+  if (!newDict[value]) {
+    newDict[value] = [];
+  }
+    newDict[value].push(key);
+}
+
+console.log(newDict);
