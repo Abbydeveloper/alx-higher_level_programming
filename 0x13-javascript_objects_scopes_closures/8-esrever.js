222208eddd8a035acec22e597bd930a35ed8ee79
Adeboga Abigail <abbyboga@gmail.com>
2024-04-09 17:10:43 -0700

    Esrever

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+let len = list.length - 1;
+
+  for (let i = 0; (len - i) > 0; i++, len--) {
+    const temp = list[i];
+    list[i] = list[len];
+    list[len] = temp;
+  }
+  return list;
+};
