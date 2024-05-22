2b85d95684bda8f5a4e210185b72e549faf49263
Adeboga Abigail <abbyboga@gmail.com>
2024-05-22 15:44:52 -0700

    Fix styling issues

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,11 +7,11 @@ const path = process.argv[3];
 
 req(url, function (error, response, body) {
   if (error) {
-    console.log(error)
+    console.log(error);
   } else {
     fs.writeFile(path, body, 'utf-8', function (err) {
       if (error) {
-        console.log(err)
+        console.log(err);
       }
     });
   }
