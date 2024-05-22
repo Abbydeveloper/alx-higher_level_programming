9c54f6d84516e40d69b49d9cba5685442403676a
Adeboga Abigail <abbyboga@gmail.com>
2024-05-22 14:51:40 -0700

    Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const req = require('request');
+const file = process.argv[2];
+
+req(file, function(error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
