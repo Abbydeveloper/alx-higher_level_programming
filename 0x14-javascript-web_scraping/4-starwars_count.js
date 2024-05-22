37fa3a32b7a5a6091dfb20e46ea0b3d5adffe914
Adeboga Abigail <abbyboga@gmail.com>
2024-05-22 15:35:54 -0700

    Fix styling issues

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,16 +4,16 @@ const url = process.argv[2];
 const req = require('request');
 
 req(url, function (error, response, body) {
-  if (error)
+  if (error) {
     console.log(error);
-  else {
-    const films_list = JSON.parse(body).results;
+  } else {
+    const filmsList = JSON.parse(body).results;
     let characters = [];
 
-    for (const film of films_list) {
+    for (const film of filmsList) {
       characters = characters.concat(film.characters);
     }
-    const count = characters.filter( function (character) {
+    const count = characters.filter(function (character) {
       return (character.includes('18'));
     });
 
