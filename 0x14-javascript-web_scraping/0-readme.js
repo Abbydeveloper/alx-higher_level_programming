9b0b284f4bf015dcf1124f90a0667f291742723e
Adeboga Abigail <abbyboga@gmail.com>
2024-05-22 14:43:55 -0700

    Fix styling issues

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,8 +3,5 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', function (error, data) {
-	if (error)
-		console.log(error);
-	else
-		console.log(data);
+  if (error) { console.log(error); } else { console.log(data); }
 });
