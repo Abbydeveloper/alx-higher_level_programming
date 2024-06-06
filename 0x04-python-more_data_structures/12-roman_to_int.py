7e811d9faea81cf10dfb92aba04e95f7faa66944
Adeboga Abigail <abbyboga@gmail.com>
2024-06-05 19:29:15 -0700

    Fix?

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,14 +4,14 @@ def roman_to_int(roman_string):
 
     if (not roman_string) or (type(roman_string) is not str):
         return (0)
-    roman_dict = {'I' : 1, 
-            'V' : 5,
-            'X' : 10,
-            'L' : 50,
-            'C' : 100,
-            'D' : 500,
-            'M' : 1000
-            }
+    roman_dict = {'I': 1,
+                  'V': 5,
+                  'X': 10,
+                  'L': 50,
+                  'C': 100,
+                  'D': 500,
+                  'M': 1000
+                  }
 
     num = 0
     prev_num = 0
@@ -19,7 +19,7 @@ def roman_to_int(roman_string):
 
     for char in roman_string:
 
-        if (prev_num  < roman_dict[char] and prev_num != 0):
+        if (prev_num < roman_dict[char] and prev_num != 0):
             num += (roman_dict[char] - (2 * prev_num))
         else:
             num += roman_dict[char]
