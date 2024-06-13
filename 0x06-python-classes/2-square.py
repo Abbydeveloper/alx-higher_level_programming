43ab90a68f66dbfd527d9e9977da3eb906086cf9
Adeboga Abigail <abbyboga@gmail.com>
2024-06-13 04:38:18 -0700

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" Class Square """
+
+
+class Square:
+    """ A Square Class """
+
+    def __init__(self, size=0):
+        """
+            Instantiate a square object
+
+            Args:
+            size (int) - size of the square
+        """
+
+        if (type(size) != int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
