0d6c66f47d9a51f29878c557cf9d312ffd429eae
Adeboga Abigail <abbyboga@gmail.com>
2024-06-13 04:53:32 -0700

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+""" Class Square """
+
+
+class Square:
+    """ A Square Class """
+
+    def __init__(self, size=0):
+        """
+            Instantiate a square object
+
+            Args:
+            size (int) - size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get or set property size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('value must be an integer')
+        elif value < 0:
+            raise ValueError('value must be >= 0')
+
+        self.__size = value
+
+    def area(self):
+        """
+            Calculate the area of the square
+        """
+        area = self.__size * self.__size
+        return (area)
