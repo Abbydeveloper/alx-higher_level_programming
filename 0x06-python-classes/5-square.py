2f2fb50a9c7dbc72e90aadb4797d13f60d1f2f1a
Adeboga Abigail <abbyboga@gmail.com>
2024-06-13 05:54:07 -0700

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+""" Class Square """
+
+
+class Square:
+    """ A Square Class """
+
+    def __init__(self, size=0):
+        """
+            Instantiate a square object
+
+            Args:
+            size (int) - size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get or set property size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = value
+
+    def area(self):
+        """
+            Calculate the area of the square
+        """
+        area = self.__size * self.__size
+        return (area)
+
+    def my_print(self):
+        """Print square"""
+        for x in range(self.__size):
+            for y in range(self.__size):
+                print('#', end="")
+            print("")
+
+        if self.__size == 0:
+            print("")
