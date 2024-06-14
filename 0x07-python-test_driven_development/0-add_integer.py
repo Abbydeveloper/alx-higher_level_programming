6c311f0f1934be6154db566db334d491c5075cc9
Adeboga Abigail <abbyboga@gmail.com>
2024-06-14 03:17:20 -0700

    Test add integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,8 +4,20 @@
 
 
 def add_integer(a, b=98):
+    """
+    Function to add two numbers
+
+    Floating numbers are typecasted to integers before addition
+
+    Args:
+    a (int): first number to add
+    b (int): second number to add
+
+    Raises:
+    TypeError: if either args are not floating numbers or integers
+    """
     if not ((isinstance(a, int) or isinstance(a, float))):
         raise TypeError('a must be an integer')
-    if not((isinstance(b, int) or isinstance(b, float))):
+    if not ((isinstance(b, int) or isinstance(b, float))):
         raise TypeError('b must be an integer')
     return (int(a) + int(b))
