d41f813362d148ed944e2c7d74c0dc29099df7c9
Adeboga Abigail <abbyboga@gmail.com>
2024-06-17 07:35:15 -0700

    String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -59,6 +59,6 @@ class Rectangle():
         for i in range(self.__height):
             for j in range(self.__width):
                 rectangle.append('#')
-            if i < self.__height:
+            if i < self.__height - 1:
                 rectangle.append('\n')
         return ("".join(rectangle))
