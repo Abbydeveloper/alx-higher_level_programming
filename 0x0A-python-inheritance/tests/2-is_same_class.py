b5c496595674702d4c58073b58c18278000dcbef
Adeboga Abigail <abbyboga@gmail.com>
2024-06-19 04:32:02 -0700

    Exact same object

---
+++ 0x0A-python-inheritance/tests/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Check object BaseClass"""
+
+
+def is_same_class(obj, a_class):
+    """Check if the oject is exactly an instance of the specified class
+    """
+    return (type(obj) == a_class)
