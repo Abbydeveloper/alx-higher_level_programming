3b2605600aba4829424c285bd8509610deb8ddf6
Adeboga Abigail <abbyboga@gmail.com>
2024-06-19 04:59:21 -0700

    Same class or inherit from

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,7 +4,8 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """Check if the oject is exactly an instance of the specified class or inherites from the specified class
+    """Check if the oject is exactly an instance of the specified class
+    or inherites from the specified class
     """
     if (isinstance(obj, a_class)):
         return (True)
