b3c1c4b64f1aad533a325701b62f02c19a547466
Adeboga Abigail <abbyboga@gmail.com>
2024-06-20 16:20:34 -0700

    Write file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,6 @@
 
 def write_file(filename="", text=""):
     """Function to write a string to a text file"""
-    
+
     with open(filename, mode="w", encoding="utf-8") as f:
         return f.write(text)
