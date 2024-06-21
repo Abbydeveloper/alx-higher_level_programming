765d54a31c1b15b0c9be01b8ef8800aa119b1f66
Adeboga Abigail <abbyboga@gmail.com>
2024-06-21 13:28:24 -0700

    Add item

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,5 +11,5 @@ def save_to_json_file(my_obj, filename):
     using a JSON representation
     """
 
-    with open(filename, mode="a") as f:
+    with open(filename, mode="w+") as f:
         json.dump(my_obj, f)
