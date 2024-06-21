0b35556fd2661a68f6f23e9597bbcddf6359a745
Adeboga Abigail <abbyboga@gmail.com>
2024-06-21 12:59:31 -0700

    Save to file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""5-save_to_json_file.py Module"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Write an object to a text file
+    using a JSON representation
+    """
+
+    with open(filename, mode="w") as f:
+        f.write(json.dumps(my_obj))
