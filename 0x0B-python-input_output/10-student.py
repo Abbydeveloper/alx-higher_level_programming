42f3e95a0ebf966d7cb225276896a12cd64b5898
Adeboga Abigail <abbyboga@gmail.com>
2024-06-22 01:15:32 -0700

    Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -13,7 +13,13 @@ class Student():
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Get the dictionary representation of a student instance"""
 
+        str_dict = dict()
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            for x in attrs:
+                if x in self.__dict__:
+                    str_dict.update({x: self.__dict__[x]})
+                return str_dict
         return self.__dict__
