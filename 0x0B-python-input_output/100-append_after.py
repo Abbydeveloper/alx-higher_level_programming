a314f773ffc4e38cd8d6611d37892b9bb2186841
Adeboga Abigail <abbyboga@gmail.com>
2024-06-22 02:22:58 -0700

    Search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""100-append_after.py Module"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Insert a line of text to a file, after each line containing
+    a specific string"""
+
+    with open(filename, mode="r") as f:
+        text = f.readlines()
+
+    with open(filename, mode="w") as fa:
+        string = ""
+        for line in text:
+            string += line
+            if search_string in line:
+                string += new_sting
+        fa.write(s)
