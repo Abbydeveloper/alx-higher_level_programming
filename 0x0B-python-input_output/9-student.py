568fa1ffd8d5b42c37a42ad1bda9335e3ff039de
Adeboga Abigail <abbyboga@gmail.com>
2024-06-22 00:44:19 -0700

    Student class

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,9 +9,9 @@ class Student():
     def __init__(self, first_name, last_name, age):
         """Initialize the student class"""
 
-        self.__first_name = first_name
-        self.__last_name = last_name
-        self.__age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """Get the dictionary representation of a student instance"""
