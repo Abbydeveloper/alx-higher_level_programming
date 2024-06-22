7a73d54810dbf92661c0327d4b3acbad84299a87
Adeboga Abigail <abbyboga@gmail.com>
2024-06-22 02:08:01 -0700

    Pascal's triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -12,12 +12,12 @@ def pascal_triangle(n):
     if n <= 0:
         return n_list
 
-    n_list = [[1]]]
+    n_list = [[1]]
 
     for x in range(1, n):
         row = [1]
         for y in range(1, x):
-            row.append(triangle[x-1][y-1] + triangle[x-1][y])
+            row.append(n_list[x-1][y-1] + n_list[x-1][y])
         row.append(1)
-        triangle.append(row)
-    return triangle
+        n_list.append(row)
+    return n_list
