1706ca34338b4109fe85f59f2dbe3d495bae28bc
Adeboga Abigail <abbyboga@gmail.com>
2024-06-28 04:46:45 -0700

    And now, the Square!

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,6 +12,23 @@ class Square(Rectangle):
         self.size = size
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Retrieve the value of size"""
+
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        """Set the value of the size attribute"""
+
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
+        self.__width = value
+        self.__height = value
+
     def __str__(self):
         """String representation of a Square instance"""
 
