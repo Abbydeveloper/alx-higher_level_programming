d0ff76d1d5865eb44bd690c285bfd96dd8f4abe0
Adeboga Abigail <abbyboga@gmail.com>
2024-06-28 03:59:05 -0700

    Update #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -104,3 +104,20 @@ class Rectangle(Base):
 
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(
                 self.id, self.__x, self.__y, self.__width, self.__height))
+
+    def update(self, *args):
+        """update value of a Rectangle insance"""
+
+        if args is not None and len(args) != 0:
+            if len(args) >= 1:
+                if not isinstance(args[0], int) and args[0] is not None:
+                    raise TypeError("id must be an integer")
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
