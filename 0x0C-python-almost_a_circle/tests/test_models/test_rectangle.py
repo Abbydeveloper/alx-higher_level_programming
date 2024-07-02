8d5b5c5525c960b90ac12dc3e16be0831fbd6df3
Adeboga Abigail <abbyboga@gmail.com>
2024-07-02 12:16:19 -0700

    Add tests for Rectangle class

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Defines unittests for the Rectangle class"""
+
+import io
+import sys
+import unittest
+import models.base import Base
+import models.base import Rectangle
+
+
+class Test_Rectangle(unittest.TestCase):
+    """Unittests for the Rectangle class"""
+
+    def test_rectangle(self):
+        """All tests for the Rectangle class"""
+
+        self.assertIsInstance(Rectangle(10, 3), Base)
+
+        with self.assertRaises(TypeError):
+            Rectangle()
+        with self.assertRaises(TypeError):
+            Rectangle(3)
+
+        rect1= Rectangle(30, 24)
+        rect2 = Rectangle(23, 12)
+        self.assertEqual(rect1.id, rect2.id - 1)
+
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle(None, 20)
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            Rectangle(-3, 4)
+
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            Rectangle(34, None)
+        with self.assertRaisesRegex(ValueError, "height must be > 0"):
+            Rectangle(2, 0)
+
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Rectangle(1, 2, None)
+        with self.assertRaisesReges(ValueError, "x must be >= 0"):
+            Rectangle(3, 2, -3, 0)
+
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Rectangle(4, 2, 4, None)
+        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
+            Rectangle(4, 2, 4, -3)
