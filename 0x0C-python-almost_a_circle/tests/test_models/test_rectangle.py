6bdd5521c8914ed8bb63a20c325beaebf76ab6d5
Adeboga Abigail <abbyboga@gmail.com>
2024-07-03 05:23:36 -0700

    Add more tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,7 +2,7 @@
 """Defines unittests for the Rectangle class"""
 
 import io
-import sys
+import contextlib
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
@@ -29,6 +29,8 @@ class Test_Rectangle(unittest.TestCase):
             Rectangle(None, 20)
         with self.assertRaisesRegex(ValueError, "width must be > 0"):
             Rectangle(-3, 4)
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            Rectangle(0, 2)
 
         with self.assertRaisesRegex(TypeError, "height must be an integer"):
             Rectangle(34, None)
@@ -41,6 +43,64 @@ class Test_Rectangle(unittest.TestCase):
             Rectangle(3, 2, -3, 0)
 
         with self.assertRaisesRegex(TypeError, "y must be an integer"):
-            Rectangle(4, 2, 4, None)
+            Rectangle(4, 2, 4, None, 5)
         with self.assertRaisesRegex(ValueError, "y must be >= 0"):
-            Rectangle(4, 2, 4, -3)
+            Rectangle(4, 2, 4, -3, 5)
+
+
+        """Test area method"""
+
+        rect = Rectangle(3, 2)
+        self.assertEqual(rect.area(), 6)
+
+        rect1 = Rectangle(10, 5)
+        self.assertEqual(rect1.area(), 50)
+
+        """Test __str__ Method"""
+
+        f = io.StringIO()
+        rect = Rectangle(4, 6, 2, 1, 12)
+        with contextlib.redirect_stdout(f):
+            print(rect)
+        string = f.getvalue()
+        response = "[Rectangle] (12) 2/1 - 4/6\n"
+        self.assertEqual(response, string)
+
+        """Test display method (without x and y, and without y also)"""
+
+        f = io.StringIO()
+        rect = Rectangle(5, 3)
+        with contextlib.redirect_stdout(f):
+            rect.display()
+        string = f.getvalue()
+        response = "#####\n#####\n#####\n"
+        self.assertEqual(string, response)
+
+        """with self.assertRaisesRegex(TypeError, 'display() takes 1 positional argument but 2 were given'):
+            rect = Rectangle(9, 6)
+            rect.display(9)"""
+
+        """Test to_dictionary method"""
+
+        rect = Rectangle(10, 2, 1, 9)
+        """Test Update"""
+
+        rect = Rectangle(10, 10, 10, 10)
+        rect.update(89)
+        self.assertEqual(89, rect.id)
+        rect.update(89, 2)
+        self.assertEqual(2, rect.width)
+        rect.update(89, 2, 3)
+        self.assertEqual(3, rect.height)
+        rect.update(89, 2, 3, 4)
+        self.assertEqual(4, rect.x)
+        rect.update(89, 2, 3, 4, 5)
+        self.assertEqual(5, rect.y)
+        rect.update()
+        self.assertEqual(str(rect), "[Rectangle] (89) 4/5 - 2/3")
+        rect.update(width=1, height=2, x=3, y=4)
+        self.assertEqual(1, rect.width)
+        self.assertEqual(2, rect.height)
+        self.assertEqual(3, rect.x)
+        self.assertEqual(4, rect.y)
+        """Test Rectangle.create()"""
