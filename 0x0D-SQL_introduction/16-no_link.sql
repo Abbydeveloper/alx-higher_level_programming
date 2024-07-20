4517571edf36b030fb1f8a46b475ada30d0fda86
Adeboga Abigail <abbyboga@gmail.com>
2024-07-20 15:08:30 -0700

    Say my name

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- list all records of the table second_table
+-- Don't list rows without a name value
+-- Resulsts should display the score and the name (in this order)
+-- Records should be listed by descending score
+SELECT score, name FROM second_table WHERE name != "" ORDER BY score DESC;
