0bd9c3caa546d09b531c9503ac0d43d945ea9868
Adeboga Abigail <abbyboga@gmail.com>
2024-07-24 14:38:36 -0700

    Cities of California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 --lists all Californian cities found in the database hbtn_0d_usa
-SELECT id, name FROM cities WHERE state_id = (SELECT id from states name = "California") Order by ID ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states name = "California") ORDER BY id ASC;
