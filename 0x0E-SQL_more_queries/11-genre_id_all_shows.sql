b4ec4c19097a9216cf974585131d83fda319c329
Adeboga Abigail <abbyboga@gmail.com>
2024-07-29 02:28:08 -0700

    No genre

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,2 @@
 -- list all shows and rows of tables contained in the db hbtn_0d_tvshows
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tb_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_shows_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_shows_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
