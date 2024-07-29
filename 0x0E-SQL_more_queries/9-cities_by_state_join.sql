de5b5c99ee20d45dab821eb80a8a7c61a4f06309
Adeboga Abigail <abbyboga@gmail.com>
2024-07-29 01:54:25 -0700

    Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- list all cities contained in the database
-SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id=cities.states_id ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
