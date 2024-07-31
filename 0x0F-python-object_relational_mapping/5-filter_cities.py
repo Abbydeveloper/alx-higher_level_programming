f384b439dff43fb54f8b9d2fbd0f713d8c5d1df6
Adeboga Abigail <abbyboga@gmail.com>
2024-07-31 14:22:48 -0700

    List cities by state name entered in user input

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""  Lists Cities by States  """
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db_cursor = db.cursor()
+    match = sys.argv[4]
+    db_cursor.execute("""SELECT cities.name FROM cities INNER JOIN states ON
+                      states.id=cities.state_id
+                      WHERE states.name=%s""", (match, ))
+    rows = db_cursor.fetchall()
+    for row in rows:
+        print(row)
+    db_cursor.close()
+    db.close()
