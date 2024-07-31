3c90088e608931c6279c592bdd54b601c04db21c
Adeboga Abigail <abbyboga@gmail.com>
2024-07-31 13:37:28 -0700

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""States Filter"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db_cursor = db.cursor()
+    db_cursor.execute("""SELECT * FROM states WHERE name
+                      LIKE BINARY 'N%' ORDER BY states.id""")
+    rows = db_cursor.fetchall()
+    for row in rows:
+        print(row)
+    db_cursor.close()
+    db.close()
