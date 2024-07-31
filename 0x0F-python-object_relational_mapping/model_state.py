bace646223831b4d97be54801853272c7b0fb34f
Adeboga Abigail <abbyboga@gmail.com>
2024-07-31 14:40:14 -0700

    Model state

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" State module """
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    """ State Class """
+    __tablename__ = 'states'
+    id = Column(Integr, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
