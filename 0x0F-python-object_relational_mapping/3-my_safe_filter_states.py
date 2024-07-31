7c135ba4c9017bafaf5f90a09634208ba3821281
Adeboga Abigail <abbyboga@gmail.com>
2024-07-31 13:55:38 -0700

    Safe state filter

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Safe state filter """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.qargv[2], db=sys.argv[3], port=3306)
+    db_cursor = db.cursor()
+    match = sys.argv[4]
+    db_cursor.execute("SELECT * FROM states WHERE name like %s", (match, ))
+    rows = db.fetchall()
+    for row in rows:
+        print(row)
+    db_curser.close()
+    db.close()
