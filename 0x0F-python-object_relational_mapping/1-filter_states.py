e4d9cfd151f21cdb6aab602bed98216c6dabbfec
Adeboga Abigail <abbyboga@gmail.com>
2024-07-31 13:47:29 -0700

    Filter states by user input

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     db_cursor = db.cursor()
-    db_cursor.execute("SELECT * FROM states WHERE name
-                      LIKE BINARY 'N%' ORDER BY states.id")
+    db_cursor.execute("""SELECT * FROM states WHERE name
+                      LIKE BINARY 'N%' ORDER BY states.id""")
     rows = db_cursor.fetchall()
     for row in rows:
         print(row)
