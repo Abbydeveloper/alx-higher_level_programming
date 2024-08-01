7adb89ce2494c3cc4de2e54951913f7344fb3259
Adeboga Abigail <abbyboga@gmail.com>
2024-08-01 03:00:31 -0700

    Create state and city

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+    State module inherits from declarative_base()
+    Create state table
+"""
+
+
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    """ State Class """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="states")
