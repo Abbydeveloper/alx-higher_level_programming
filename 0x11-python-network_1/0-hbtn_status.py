984571c82c7b23aa7ff72162c0016ef968148ef8
Adeboga Abigail <abbyboga@gmail.com>
2024-08-26 12:15:51 -0700

    What is my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/bin/env/python3
+#Fetch https://alx-intranet.hbtn.io/status
+
+
+if __name__ == "__main__":
+    from urllib import request
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        text = resp.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(text)))
+        print('\t- text: {}'.format(text))
+        print('\t utf8 content: {}'.format(content.decode('utf-8')))
