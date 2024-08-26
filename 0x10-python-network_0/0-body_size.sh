7abf11cf14588c234d9079584466119e3de9d5f5
Adeboga Abigail <abbyboga@gmail.com>
2024-08-26 04:28:15 -0700

    Get size of response in bytes

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Take in a url, send a reques to the url and display the size of the boddy
-# of the response
-
+# Take in a url, send a reques to the url and display the size of the boddy of the response
 curl -s "$1" | wc -c
