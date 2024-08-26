6570433077f31aee426da2053e72679f3d064b4b
Adeboga Abigail <abbyboga@gmail.com>
2024-08-26 12:47:07 -0700

    Response header value #0

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Take a URL from command line argument, and display the value of
+    the X-Request-Id variable found in the header of the response"""
+
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request
+
+    url = sys.argv[1]
+    with request.urlopen(url) as resp:
+        print(dict(resp.headers).get('X.Request-Id'))
