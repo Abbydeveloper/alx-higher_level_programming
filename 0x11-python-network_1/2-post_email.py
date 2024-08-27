55ac4b1b172c953044b3801ff25b41aded226ae0
Adeboga Abigail <abbyboga@gmail.com>
2024-08-27 07:59:35 -0700

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Take a URL and an email, send a POST request to the passed URL with the email as a paramet, and display the body of the response"""
+"""Take a URL and an email, send a POST request to the passed URL with the
+email as a parameter, and display the body of the response"""
 
 
 if __name__ == "__main__":
