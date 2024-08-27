2f9cea5f8da9a5af70f1e4d814e59eecd4afaf8f
Adeboga Abigail <abbyboga@gmail.com>
2024-08-27 08:38:57 -0700

    Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Take a URL and an email, send a POST request to the passed URL with the
+email as a parameter, and display the body of the response"""
+
+
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+    import urllib.parse
+
+    url = sys.argv[1]
+
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as resp:
+            body = resp.read()
+            print(body.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print('Error code" '.format(e.code))
