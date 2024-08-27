dae5b5b852904db73941c5b5a6ce9074ffb8a4fc
Adeboga Abigail <abbyboga@gmail.com>
2024-08-27 08:40:17 -0700

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -16,4 +16,4 @@ if __name__ == "__main__":
             body = resp.read()
             print(body.decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print('Error code" '.format(e.code))
+        print('Error code: {}'.format(e.code))
