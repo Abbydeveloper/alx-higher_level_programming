36a16cbc1382324174e8589a470acc0c4b7c600b
Adeboga Abigail <abbyboga@gmail.com>
2024-08-28 11:07:10 -0700

    Gitub commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""List the 10 most recent commints from any given GitHub repository"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}.commits"
+           .format(sys.argv[2], sys.argv[1])
+
+    req = requests.get(url)
+    commits = req.json()
+    try:
+        for i in range(10):
+            print(f"{commits[i]['sha']}: {commits[i]['commit']['autor']['name'[}")
+    except IndexError:
+        pass
