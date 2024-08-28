1f43ad74e3834d25e33c6efae48b62d9623b1c24
Adeboga Abigail <abbyboga@gmail.com>
2024-08-28 11:02:01 -0700

    My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,6 +7,6 @@ from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    auth - HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     req = requests.get("https://api.github.com/user", auth)
     print(req.json().get("id"))
