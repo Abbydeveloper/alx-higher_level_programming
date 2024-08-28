3c657b2231b647ac1a903c50d56edc9afa1515c8
Adeboga Abigail <abbyboga@gmail.com>
2024-08-28 11:00:12 -0700

    My GitHub!

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Take github credentials and use the Github APPI to display ID"""
+
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth - HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    req = requests.get("https://api.github.com/user", auth)
+    print(req.json().get("id"))
