56f7926a24630437b6b624da6b6536bf97cdf02e
Adeboga Abigail <abbyboga@gmail.com>
2024-08-28 11:24:33 -0700

    Fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,12 +6,13 @@ import requests
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}.commits".format(
+    url = "https://api.github.com/repos/{}/{}/commits".format(
            sys.argv[2], sys.argv[1])
 
     req = requests.get(url)
     commits = req.json()
     try:
+        print(commits)
         for i in range(10):
             print("{}: {}".format(
                   commits[i].get("sha"),
