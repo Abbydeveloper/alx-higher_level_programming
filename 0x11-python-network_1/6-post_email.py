c6480c04429bc9c4d68d68b9947717dc1371b52e
Adeboga Abigail <abbyboga@gmail.com>
2024-08-28 10:22:45 -0700

    POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Take in a URL and an email address, send a post request to the 
+URL with the email as a parameter and display the body of the response"""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = requests.get(url, {'email': sys.argv[2])
+    print(req.text)
